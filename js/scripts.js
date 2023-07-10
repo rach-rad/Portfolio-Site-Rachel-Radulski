d9808bd8cd4b2acd506f7e3f772e0439ef98d4d2
rach-rad <125483072+rach-rad@users.noreply.github.com>
2023-07-10 11:53:53 -0400

    adda js interactivity

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Hello world");                 // prints the given text to the console
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+    } else {
+    x.className = 'navtoggle';
+    }
+}
+  
