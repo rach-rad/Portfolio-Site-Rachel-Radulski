f46d97fe2ab40baf5bd597986114c2ea0714d1ce
rach-rad <125483072+rach-rad@users.noreply.github.com>
2023-07-17 17:05:46 -0400

    add styling to navigation and links

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 console.log("Hello world");                 // prints the given text to the console
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
     x.className += ' responsive';
-    } else {
+  } else {
     x.className = 'navtoggle';
-    }
+  }
 }
   
-// Get the button:
-let mybutton = document.getElementById("myBtn");
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -27,4 +29,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}
